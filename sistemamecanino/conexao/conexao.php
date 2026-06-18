027cdd4c5619ef203cbbf9d792e5e19b871d9312
viniciusenoque-commits <vinicius.enoque@aluno.ifsertao-pe.edu.br>
2026-06-18 15:31:47 -0300

    Update conexao.php

--- sistemamecanino/conexao/conexao.php
+++ sistemamecanino/conexao/conexao.php
@@ -602,5 +602,5 @@ try {
     die("Erro de conexão PDO crítico: " . $e->getMessage());
 }
 
-define('BASE_URL', 'http://localhost/oficinamecanica/index.php');
+define('BASE_URL', 'http://localhost/OficinaMecanica/sistemamecanino/index.php');
 ?>
