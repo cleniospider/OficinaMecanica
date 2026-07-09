1e32de25a4da5f5db46e89fd6d8a75e5e60d14cd
cleniospider <clenio.lopes@aluno.ifsertao-pe.edu.br>
2026-07-09 15:52:43 -0300

    a

--- sistemamecanino/minha-conta-recep.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Redireciona para a página de conta unificada
-require_once('conexao/conexao.php');
-if (!isset($_SESSION['usuario_id'])) {
-    header("Location: index.php");
-    exit;
-}
-header("Location: minha-conta.php");
-exit;
